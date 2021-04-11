11a71db058a7bd69c125cea5b8425d17fe9e5eca
Björn Kahlert <mail@bkahlert.com>
2021-04-11 05:12:27 +0200

    refactor: allow disabling parameter substitution for heredocs

--- src/jvmMain/kotlin/koodies/shell/HereDoc.kt
+++ src/jvmMain/kotlin/koodies/shell/HereDoc.kt
@@ -3,41 +3,63 @@ package koodies.shell
 import koodies.text.CharRanges
 import koodies.text.LineSeparators
 import koodies.text.randomString
+import koodies.text.singleQuoted
 
 /**
- * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of the given [lines], a customizable [label] and [lineSeparator].
+ * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of the given [commands], a customizable [delimiter] and [lineSeparator].
  */
 public class HereDoc(
     /**
+     * Lines of text this here document is made of.
+     */
+    public vararg val commands: String,
+
+    /**
      * Identifier to delimit this here document from the surrounding text.
      */
-    public val label: String = randomLabel(),
+    public val delimiter: String = randomDelimiter(),
+
+    /**
+     * Whether the shell will process this heredoc like any other input,
+     * in particular substitution will take place.
+     *
+     * If disabled, the content will be treated as is, i.e. `$HOME` will no be substituted.
+     *
+     * @see <a href="https://tldp.org/LDP/abs/html/here-docs.html">Advanced Bash-Scripting Guide: Chapter 19. Here Documents</a>
+     */
+    public val substituteParameters: Boolean = true,
+
     /**
      * Separator used to separator the lines of this here document.
      */
+    @Deprecated("unused feature; remove")
     public val lineSeparator: String = DEFAULT_LINE_SEPARATOR,
-    /**
-     * Lines of text this here document is made of.
-     */
-    public val lines: Array<String>,
 ) : CharSequence {
     /**
-     * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of the given [lines], a customizable [label] and [lineSeparator].
+     * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of the given [commands], a customizable [delimiter] and [lineSeparator].
      */
-    public constructor(label: String = randomLabel(), lineSeparator: String = DEFAULT_LINE_SEPARATOR, lines: Iterable<CharSequence>) :
-        this(label = label, lineSeparator = lineSeparator, lines = lines.map { "$it" }.toTypedArray())
+    public constructor(commands: Iterable<CharSequence>, delimiter: String = randomDelimiter(), substituteParameters: Boolean = true) :
+        this(*commands.map { it.toString() }.toTypedArray<String>(), delimiter = delimiter, substituteParameters = substituteParameters)
+
+    /**
+     * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of the given [lines], a customizable [delimiter] and [lineSeparator].
+     */
+    @Deprecated("unused feature; remove")
+    public constructor(delimiter: String = randomDelimiter(), lineSeparator: String = DEFAULT_LINE_SEPARATOR, lines: Iterable<CharSequence>) :
+        this(commands = lines.map { "$it" }.toTypedArray(), delimiter = delimiter, lineSeparator = lineSeparator)
 
     /**
      * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of the given [lines], a customizable [label] and [lineSeparator].
      */
-    public constructor(vararg lines: CharSequence, label: String = randomLabel(), lineSeparator: String = DEFAULT_LINE_SEPARATOR) :
-        this(label = label, lineSeparator = lineSeparator, lines = lines.map { "$it" })
+    @Deprecated("unused feature; remove")
+    public constructor(vararg lines: CharSequence, label: String = randomDelimiter(), lineSeparator: String = DEFAULT_LINE_SEPARATOR) :
+        this(delimiter = label, lineSeparator = lineSeparator, lines = lines.map { "$it" })
 
     public companion object {
         /**
          * Returns a random—most likely unique—label to be used for a [HereDoc].
          */
-        public fun randomLabel(): String = "HERE-" + randomString(8, CharRanges.UpperCaseAlphanumeric)
+        public fun randomDelimiter(): String = "HERE-" + randomString(8, CharRanges.UpperCaseAlphanumeric)
 
         /**
          * The line separator used by default to separate lines in a [HereDoc].
@@ -45,7 +67,11 @@ public class HereDoc(
         public const val DEFAULT_LINE_SEPARATOR: String = LineSeparators.LF
     }
 
-    private val rendered = sequenceOf("<<$label", *lines, label).joinToString(separator = lineSeparator)
+    private val rendered = sequenceOf(
+        "<<${delimiter.takeIf { substituteParameters } ?: delimiter.singleQuoted}",
+        *commands,
+        delimiter,
+    ).joinToString(separator = lineSeparator)
 
     override val length: Int = rendered.length
     override fun get(index: Int): Char = rendered[index]
@@ -62,25 +88,17 @@ public class HereDoc(
 }
 
 /**
- * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of the given [lines], a customizable [label] and [lineSeparator].
+ * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of this collection of commands
+ * and the optionally configurable [delimiter].
  */
-public fun <T : CharSequence> List<T>.toHereDoc(
-    label: String = HereDoc.randomLabel(),
-    lineSeparator: String = HereDoc.DEFAULT_LINE_SEPARATOR,
-): HereDoc = HereDoc(label = label, lineSeparator = lineSeparator, lines = this)
+public fun Iterable<CharSequence>.toHereDoc(
+    delimiter: String = HereDoc.randomDelimiter(),
+): HereDoc = HereDoc(delimiter = delimiter, commands = this)
 
 /**
- * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of the given [lines], a customizable [label] and [lineSeparator].
+ * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of this array of commands
+ * and the optionally configurable [delimiter].
  */
-public fun <T : CharSequence> Iterable<T>.toHereDoc(
-    label: String = HereDoc.randomLabel(),
-    lineSeparator: String = HereDoc.DEFAULT_LINE_SEPARATOR,
-): HereDoc = HereDoc(label = label, lineSeparator = lineSeparator, lines = this)
-
-/**
- * Creates a [here document](https://en.wikipedia.org/wiki/Here_document) consisting of the given [lines], a customizable [label] and [lineSeparator].
- */
-public fun <T : CharSequence> Array<T>.toHereDoc(
-    label: String = HereDoc.randomLabel(),
-    lineSeparator: String = HereDoc.DEFAULT_LINE_SEPARATOR,
-): HereDoc = HereDoc(label = label, lineSeparator = lineSeparator, lines = this)
+public fun Array<String>.toHereDoc(
+    delimiter: String = HereDoc.randomDelimiter(),
+): HereDoc = HereDoc(commands = this, delimiter = delimiter)
--- src/jvmTest/kotlin/koodies/shell/HereDocKtTest.kt
+++ src/jvmTest/kotlin/koodies/shell/HereDocKtTest.kt
@@ -1,24 +1,34 @@
 package koodies.shell
 
+import koodies.concurrent.process.io
+import koodies.concurrent.process.merged
+import koodies.concurrent.process.out
+import koodies.concurrent.script
 import koodies.test.toStringIsEqualTo
+import koodies.text.ANSI.ansiRemoved
+import koodies.text.lines
 import koodies.text.matchesCurlyPattern
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT
 import strikt.api.expectThat
+import strikt.assertions.first
+import strikt.assertions.isEqualTo
+import strikt.assertions.isNotBlank
+import strikt.assertions.isNotEqualTo
 
 @Execution(CONCURRENT)
 class HereDocKtTest {
 
     @Test
-    fun `should create here document using given prefix and line separator`() {
-        val hereDoc = listOf("line 1", "line 2").toHereDoc("MY-PREFIX", "␤")
-        expectThat(hereDoc).toStringIsEqualTo("<<MY-PREFIX␤line 1␤line 2␤MY-PREFIX")
+    fun `should create here document using given delimiter and line separator`() {
+        val hereDoc = HereDoc("line 1", "line 2", delimiter = "MY-DELIMITER", lineSeparator = "␤")
+        expectThat(hereDoc).toStringIsEqualTo("<<MY-DELIMITER␤line 1␤line 2␤MY-DELIMITER")
     }
 
     @Test
-    fun `should create here document using HERE- prefix and line feed by default`() {
+    fun `should create here document using HERE- delimiter and line feed by default`() {
         val hereDoc = listOf("line 1", "line 2").toHereDoc()
         expectThat(hereDoc).matchesCurlyPattern("""
             <<HERE-{}
@@ -26,6 +36,51 @@ class HereDocKtTest {
             line 2
             HERE-{}
         """.trimIndent())
+    }
+
+    @Nested
+    inner class ParameterSubstitution {
+
+        @Nested
+        inner class Enabled {
+
+            private val hereDoc = HereDoc(commands = arrayOf("\$HOME"), delimiter = "TEST")
+
+            @Test
+            fun `should not change delimiter`() {
+                expectThat(hereDoc).lines().first().isEqualTo("<<TEST")
+            }
+
+            @Test
+            fun `should substitute parameters`() {
+                expectThat(script {
+                    !"cat $hereDoc"
+                }.io.out.merged.ansiRemoved) {
+                    isNotBlank()
+                    isNotEqualTo("\$HOME")
+                }
+            }
+        }
+
+        @Nested
+        inner class Disabled {
+
+            private val hereDoc = HereDoc(commands = arrayOf("\$HOME"), delimiter = "TEST", substituteParameters = false)
+
+            @Test
+            fun `should not single quote delimiter`() {
+                expectThat(hereDoc).lines().first().isEqualTo("<<'TEST'")
+            }
+
+            @Test
+            fun `should not substitute parameters`() {
+                expectThat(script {
+                    !"cat $hereDoc"
+                }.io.out.merged.ansiRemoved) {
+                    isEqualTo("\$HOME")
+                }
+            }
+        }
     }
 
     @Test
