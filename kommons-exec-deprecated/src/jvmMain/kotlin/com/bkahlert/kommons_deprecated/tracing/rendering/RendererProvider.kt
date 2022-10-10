8d830d074012640894d7f288f907897e79960186
Björn Kahlert <mail@bkahlert.com>
2022-10-11 01:20:24 +0200

    refactor(exec): remove kommons-exec-deprecated module

--- kommons-exec-deprecated/src/jvmMain/kotlin/com/bkahlert/kommons_deprecated/tracing/rendering/RendererProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.bkahlert.kommons_deprecated.tracing.rendering
-
-/**
- * A function that provides a [RendererProvider] based on
- * the given settings default provider.
- */
-public typealias RendererProvider = Settings.(default: RendererFactory) -> Renderer
