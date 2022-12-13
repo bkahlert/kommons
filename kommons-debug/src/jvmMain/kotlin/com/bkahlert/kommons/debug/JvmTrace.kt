cbbb4ef67947addf16ff0f256f9a8a4d937f59dd
Björn Kahlert <mail@bkahlert.com>
2022-12-14 00:32:44 +0100

    fix: downgrade to Gradle 7.5 due to https://youtrack.jetbrains.com/issue/KT-54634/MPP-Test-Failure-causes-KotlinJvmTestExecutorexecute1-does-not-define-failure

---
+++ kommons-debug/src/jvmMain/kotlin/com/bkahlert/kommons/debug/JvmTrace.kt
@@ -0,0 +1,68 @@
+package com.bkahlert.kommons.debug
+
+import com.bkahlert.kommons.text.LineSeparators
+
+/**
+ * Helper property that supports
+ * [print debugging][https://en.wikipedia.org/wiki/Debugging#Print_debugging].
+ *
+ * **Example**
+ * ```kotlin
+ * chain().of.endless().trace.breaks.print().calls()
+ * ```
+ * … does the same as …
+ *
+ * ```kotlin
+ * chain().of.endless().calls()
+ * ```
+ * … with the only difference that the return value of
+ *
+ * ```kotlin
+ * chain().of.endless()
+ * ```
+ *
+ * is printed.
+ */
+@Deprecated("Don't forget to remove after you finished debugging.", replaceWith = ReplaceWith("this"))
+public actual fun <T> T.trace(
+    caption: CharSequence?,
+    render: Renderer?,
+    out: Printer?,
+    inspect: Inspector<T>?,
+): T {
+    val includeCallSite = true
+    val highlight = true
+    val call = if (includeCallSite) StackTrace.get().findByLastKnownCallsOrNull(::inspect, ::trace) else null
+    val actualRender = render ?: { it.render() }
+    buildString {
+        if (call != null) {
+            append(".ͭ ")
+            append("(${call.fileName}:${call.lineNumber}) ")
+        }
+        caption?.also {
+            append(caption.let { if (highlight) it.highlighted else it })
+            append(' ')
+        }
+        appendWrapped(
+            actualRender(this@trace).let { if (highlight) it.highlightedStrongly else it },
+            if (highlight) "⟨".highlighted to "⟩".highlighted else "⟨" to "⟩",
+        )
+        inspect?.also {
+            append(" ")
+            appendWrapped(
+                actualRender(inspect(this@trace)).let { if (highlight) it.highlightedStrongly else it },
+                if (highlight) "{".highlighted to "}".highlighted else "{" to "}",
+            )
+        }
+    }.also { out?.invoke(it) ?: println(it) }
+    return this
+}
+
+
+/** This string cyan colored and emphasized. */
+public val CharSequence.highlighted: String
+    get() = lineSequence().joinToString(LineSeparators.LF) { "\u001b[1;36m$it\u001B[0m" }
+
+/** This string bright cyan colored. */
+public val CharSequence.highlightedStrongly: String
+    get() = lineSequence().joinToString(LineSeparators.LF) { "\u001b[96m$it\u001B[0m" }
---
+++ kommons-debug/src/jvmMain/kotlin/com/bkahlert/kommons/debug/StackTrace.kt
@@ -0,0 +1,126 @@
+package com.bkahlert.kommons.debug
+
+import com.bkahlert.kommons.EMPTY
+import java.lang.reflect.Method
+import java.util.Locale
+import kotlin.reflect.KClass
+import kotlin.reflect.KClassifier
+import kotlin.reflect.KFunction
+
+/** Representation of a stack trace. */
+public class StackTrace(elements: List<StackTraceElement>) : List<StackTraceElement> by elements {
+    override fun toString(): String {
+        return joinToString("\n    at ")
+    }
+
+    public companion object
+}
+
+/** Gets the current [StackTrace]. */
+@Suppress("NOTHING_TO_INLINE") // inline to avoid impact on stack trace
+public inline fun StackTrace.Companion.get(): StackTrace =
+    Thread.currentThread().stackTrace
+        .dropWhile { it.className == Thread::class.qualifiedName }
+        .let { StackTrace(it) }
+
+/**
+ * Finds the [StackTraceElement] that immediately follows the one
+ * the specified [predicate] stops returning `true` for the first time.
+ *
+ * In other words:
+ * - The [predicate] is used to drop irrelevant calls for as long as it returns `false`.
+ * - As soon as it responds `true` calls that are expected to exist are dropped.
+ * - Finally, the next element which represents the caller of the last matched call is returned.
+ */
+public fun StackTrace.findOrNull(predicate: (StackTraceElement) -> Boolean): StackTraceElement? =
+    (if (firstOrNull()?.let(predicate) == false) dropWhile { !predicate(it) } else this).dropWhile { predicate(it) }.firstOrNull()
+
+/**
+ * Finds the [StackTraceElement] that immediately follows the one
+ * the specified [predicate] stops returning `true` for the first time.
+ *
+ * In other words:
+ * - The [predicate] is used to drop irrelevant calls for as long as it returns `false`.
+ * - As soon as it responds `true` calls that are expected to exist are dropped.
+ * - Finally, the next element which represents the caller of the last matched call is returned.
+ */
+public fun StackTrace.find(predicate: (StackTraceElement) -> Boolean): StackTraceElement =
+    findOrNull(predicate) ?: throw NoSuchElementException()
+
+private val functionMangleRegex: Regex = "\\$.*$".toRegex()
+private val generatedFunctionRegex = "\\\$.*".toRegex()
+
+/** Returns the specified [function] with mangling information removed. */
+public fun StackTrace.Companion.demangleFunction(function: String): String =
+    function.replace(functionMangleRegex, String.EMPTY)
+
+/** The name of the invoked function with mangling information removed. */
+public val StackTraceElement.demangledFunction: String?
+    get() = StackTrace.demangleFunction(methodName)
+
+
+/** The [Class] containing the execution point represented by this element. */
+public val StackTraceElement.`class`: Class<*> get() = Class.forName(className)
+
+/** The [KClass] containing the execution point represented by this element. */
+public val StackTraceElement.kClass: KClass<*> get() = `class`.kotlin
+
+
+/** The method containing the execution point represented by this element. */
+public val StackTraceElement.method: Method get() = `class`.declaredMethods.single { it.name == methodName }
+
+/**
+ * Finds the [StackTraceElement] that represents the caller
+ * invoking the [StackTraceElement] matching a call to the specified [functions].
+ */
+public fun StackTrace.findByLastKnownCallsOrNull(vararg functions: Pair<String?, String>): StackTraceElement? {
+    var skipInvoke = false
+    val demangledFunctions = functions.map { (className, function) -> className to StackTrace.demangleFunction(function) }
+    return findOrNull {
+        if (demangledFunctions.any { (className, demangledFunction) -> it.className == className && it.demangledFunction == demangledFunction }) {
+            skipInvoke = true
+            true
+        } else {
+            if (skipInvoke) it.methodName == "invoke" else false
+        }
+    }
+}
+
+/**
+ * Finds the [StackTraceElement] that represents the caller
+ * invoking the [StackTraceElement] matching a call to the specified [className] and [function].
+ */
+public fun StackTrace.findByLastKnownCallsOrNull(className: KClassifier, function: String): StackTraceElement? =
+    findByLastKnownCallsOrNull(className.toString() to function)
+
+private fun String.javaBeanMethodToKotlinProperty() = takeUnless {
+    length >= 4 && substring(0, 3).let { it == "get" || it == "set" } && this[3].isUpperCase()
+} ?: substring(3).replaceFirstChar { it.lowercase(Locale.getDefault()) }
+
+private fun String.simplifyFunction() =
+    replace(generatedFunctionRegex, String.EMPTY).javaBeanMethodToKotlinProperty()
+
+/**
+ * Finds the [StackTraceElement] that represents the caller
+ * invoking the [StackTraceElement] matching a call to the specified [functions].
+ */
+public fun StackTrace.findByLastKnownCallsOrNull(vararg functions: String): StackTraceElement? {
+    var skipInvoke = false
+    val simplifiedFunctions = functions.map { StackTrace.demangleFunction(it).simplifyFunction() }
+
+    return findOrNull {
+        if (simplifiedFunctions.contains(it.demangledFunction?.simplifyFunction())) {
+            skipInvoke = true
+            true
+        } else {
+            if (skipInvoke) it.methodName == "invoke" else false
+        }
+    }
+}
+
+/**
+ * Finds the [StackTraceElement] that represents the caller
+ * invoking the [StackTraceElement] matching a call to the specified [functions].
+ */
+public fun StackTrace.findByLastKnownCallsOrNull(vararg functions: KFunction<*>): StackTraceElement? =
+    findByLastKnownCallsOrNull(*functions.map { it.name }.toTypedArray())
